57b55cc12de9fe95a804173c470995994c7e6197
Biceps <rforlyffe@gmail.com>
2019-12-15 18:08:26 +0800

    Updates

---
+++ precache-manifest.6862f7ecbf1e08a7e95f21795c7222fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f316ce758553ea73a20f079a3ce909",
+    "url": "/calendar-event/index.html"
+  },
+  {
+    "revision": "0091fefbfc6e647d6777",
+    "url": "/calendar-event/static/css/2.8f6e2acf.chunk.css"
+  },
+  {
+    "revision": "da602f380e05d8f6c6c5",
+    "url": "/calendar-event/static/css/main.a11b7641.chunk.css"
+  },
+  {
+    "revision": "0091fefbfc6e647d6777",
+    "url": "/calendar-event/static/js/2.cf1eb7dc.chunk.js"
+  },
+  {
+    "revision": "66cead6e62612ff9046002ad99f3dd77",
+    "url": "/calendar-event/static/js/2.cf1eb7dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da602f380e05d8f6c6c5",
+    "url": "/calendar-event/static/js/main.84672fae.chunk.js"
+  },
+  {
+    "revision": "b3a43471f355281c9266",
+    "url": "/calendar-event/static/js/runtime-main.c26d9d04.js"
+  },
+  {
+    "revision": "04ea6f8594862863b61e58f91566da0e",
+    "url": "/calendar-event/static/media/Calendar-planner.04ea6f85.gif"
+  }
+]);
